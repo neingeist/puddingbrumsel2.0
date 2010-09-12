acddbcb674ec209f7af873b23233c0b85649bbcd
orange <mike@sprachgewalt.de>
2010-09-12 14:27:53 +0200

    i can has generative grammar, sortof

--- pudding.py
+++ pudding.py
@@ -11,27 +11,55 @@ class puddingbrumsel:
     self.cursor.execute("SELECT word FROM words");
     self.words = map(lambda r: r[0], self.cursor.fetchall())
 
+  def pick(self, list):
+    return list[random.randint(0, len(list)-1)]
+
   def pudding(self):
-    return "pudding"
+    return self.pick([
+      "pudding",
+      "pudding",
+      self.neupudding()
+    ])
 
   def neupudding(self):
-    return self.words[random.randint(0, len(self.words)-1)] + "pudding"
+    return self.pick(self.words) + "pudding"
 
-  def brumms(self):
-    if random.randint(0, 1):
-      return "*brumms*"
-    else:
-      return "*brums*"
+  def brums(self):
+    return self.pick([
+      "*brumms*", 
+      "*brums*"
+    ])
+
+  def emoticon(self):
+    return self.pick([
+      ":D",
+      ":)",
+      "^^"
+    ])
+
+  def doubleemoticon(self):
+    return self.pick([
+      ":D :D",
+      ":))",
+      "^^ ^^"
+    ])
+
+  def sademoticon(self):
+    return self.pick([
+      ":((",
+      ":-(",
+      ":/"
+    ])
 
   def tweet(self):
-    if random.randint(0, 3) == 0:
-      tweet = self.neupudding()
-    else:
-      tweet = self.pudding()
-
-    if len(tweet) < 120:
-      tweet += self.brumms() 
-
+    return self.pick([
+      "<3 " + self.pudding() + " " + self.emoticon(),
+      self.pudding() + " " + self.doubleemoticon(),
+      self.pudding() + " " + self.brums(), 
+      self.pudding() + " " + self.brums() + " " + self.emoticon(),
+      "hihi. " + self.pudding() + " " + self.brums() +  " " + self.emoticon(),
+      "kein " + self.pudding() + " " + self.sademoticon()
+    ])
     return tweet
 
 
